0df5f11fc416ca56b921e710d849a71a0e71f162
pgrudev <1991december@gmail.com>
2016-12-11 21:57:35 +0100

    init commit

---
+++ magicEightBall/Answers.java
@@ -0,0 +1,32 @@
+package magicEightBall;
+
+public enum Answers {
+	ONE("It is certain"),
+	TWO("It is decidedly so"),
+	THREE("Without a doubt"),
+	FOUR("Yes, definitely"),
+	FIVE("You may rely on it"),
+	SIX("As I see it, yes"),
+	SEVEN("Most likely"),
+	EIGHT("Outlook good"),
+	NINE("Yes"),
+	TEN("Signs point to yes"),
+	ELEVEN("Reply hazy try again"),
+	TWELVE("Ask again later"),
+	THIRTEEN("Better not tell you now"),
+	FOURTEEN("Cannot predict now"),
+	FIVETEEN("Concentrate and ask again"),
+	SIXTEEN("Don't count on it"),
+	SEVENTEEN("My reply is no"),
+	EIGHTEEN("My sources say no"),
+	NINETEEN("Outlook not so good"),
+	TWENTY("Very doubtful");
+private final String message;
+private Answers(String message){
+	this.message=message;
+}
+
+public String getMessage(){
+	return message;
+}
+}
---
+++ magicEightBall/Raffle.java
@@ -0,0 +1,20 @@
+package magicEightBall;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+public class Raffle {
+	private List<Answers> answers;
+	private Random randomGenerator;
+	
+	public Raffle(){
+		answers = new ArrayList<Answers>(Arrays.asList(Answers.values()));
+		randomGenerator = new Random();
+	}
+	public String eightBall(){
+		int index = randomGenerator.nextInt(answers.size());
+		return answers.get(index).getMessage();
+	}
+}
